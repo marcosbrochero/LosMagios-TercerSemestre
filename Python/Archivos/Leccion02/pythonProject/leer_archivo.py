1b21580a1fee98835ec0fe6f25742044ce391a65
Gonzalo Bucca <gonzalobucca1@gmail.com>
2025-05-12 23:48:24 -0300

    Agregamos tareas de py

---
+++ Python/Archivos/Leccion02/pythonProject/leer_archivo.py
@@ -0,0 +1,21 @@
+archivo = open("prueba.txt", "r", encoding="utf8") # Las letras son: "r" read, "a" append, "w" write, "x"
+#print(archivo.read())
+#print(archivo.read(16))
+#print(archivo.read(10)) # Continuamos desde la linea anterior
+#print(archivo.readline())
+#print(archivo.readline())
+
+# vamos a iterar el archivo, cada una de las líneas
+#for linea in archivo:
+    #print(linea): iteramos todos lods elementos del archivo
+#print(archivo.readlines()[11]) #accedemos al archivo como si fuera una lista
+#Anexamos información, copiamos a otro
+archivo2 = open("copia.txt", "w", encoding="utf8")
+archivo2.write(archivo.read())
+archivo.close() # cerramos el primer archivo
+archivo2.close() # cerramos el segundo archivo
+
+print("se ha terminado del proceso de leer y copiar archivos")
+
+
+
